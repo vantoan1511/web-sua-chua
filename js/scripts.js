a029dd06949614dc4bdef70ae89529cd8bd089ba
NGUYEN VAN TOAN <vantoan1517@gmail.com>
2023-04-13 20:26:02 +0700

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -121,7 +121,8 @@ function showCart() {
         var thanhtiensp = document.createElement("td");
         thanhtiensp.innerHTML = orderNumber * price + " VND";
         var xoasp = document.createElement("td");
-        xoasp.innerHTML = "<a href='#' data-code = '" + code + "' onclick='removeCart(this.dataset.code)'><i class='fa fa-trash icon-pink icon'></i></a>";
+        xoasp.innerHTML = "<a href='#' data-code = '" + code
+            + "' onclick='removeCart(this.dataset.code)'><i class='fa fa-trash icon-pink icon'></i></a>";
         var tr = document.createElement("tr");
         tr.appendChild(hinhsp);
         tr.appendChild(tensp);
@@ -158,7 +159,101 @@ function getDiscountRate() {
     var d = new Date();
     var weekday = d.getDay();
     var totalMins = d.getHours() * 60 + d.getMinutes();
-    if (weekday >= 1 && weekday <= 3 && ((totalMins >= 420 && totalMins <= 660) || (totalMins >= 780 && totalMins <= 1020)))
+    if (weekday >= 1 && weekday <= 3 && ((totalMins >= 420 && totalMins <= 660)
+        || (totalMins >= 780 && totalMins <= 1020)))
         return 0.1;
     return 0;
 }
+
+
+//jquery
+
+$(document).ready(function () {
+
+    var d = new Date();
+    var ads = "Khách hàng có ngày sinh trong tháng " + d.getMonth()
+        + " sẽ được tặng 2 phần sữa chua dâu cho đơn hàng đầu tiên trong tháng.";
+    $('footer').append("<div id = 'adscontainer'><span id = 'adstext'><h2>" + ads + "</h2></span></div>");
+
+    var W = ($(window).width() - $('main').width()) / 2;
+    if (W >= 200) {
+        adsVerEffect();
+    }
+    else {
+        adsHorEffect();
+    }
+
+
+    function adsVerEffect() {
+        $('#adscontainer').addClass('adsvercontainer container');
+        $('#adscontainer').css('width', W);
+        $('#adstext').addClass('adsvertext adstext');
+        $('#adstext').css('top', $('#adscontainer').height());
+        $('#adstext').animate({
+            top: '-=' + ($('#adscontainer').height() + $('#adstext').height())
+        }, 20000, adsVerEffect);
+    }
+
+    function adsHorEffect() {
+        $('#adscontainer').addClass('adshorcontainer container');
+        $('#adscontainer').css('left', $('main').position().left);
+        $('#adscontainer').css('width', $('main').width());
+        $('#adstext').addClass('adshortext adstext');
+        $('#adstext').css('left', $('#adscontainer').width());
+        $('#adstext').animate({
+            left: '-=' + ($('#adscontainer').width() + $('#adstext').width())
+        }, 20000, adsHorEffect);
+    }
+
+
+    var headlineContent = [
+        {
+            'title': 'Bánh flan sữa chua sự kết hợp hoàn hảo',
+            'photo': '/images/trangchu/headline/headline1.jpg'
+        },
+        {
+            'title': 'Sữa chua nhà làm từ sữa dê - đậm đà hương vị khó quên',
+            'photo': '/images/trangchu/headline/headline2.jpg'
+        },
+        {
+            'title': ' Thưởng thức sữa chua theo cách của bạn',
+            'photo': '/images/trangchu/headline/headline3.jpg'
+        }
+    ];
+
+    var divContent = new Array();
+    var divHeadline = $('#headline');
+    
+    function init() {
+
+        for (var i = 0; i < headlineContent.length; i++) {
+            divContent[i] = $('<div>');
+            divContent[i].html('<span><h3>' + headlineContent[i].title
+                + '</h3></span><img src = "' + headlineContent[i].photo + '"></img>');
+            divHeadline.append(divContent[i]);
+        }
+    }
+
+    function doHeadline() {
+
+        init();
+
+        $('#headline div:nth-child(n+2)').hide();
+
+        setInterval(function () {
+            var firstElement = divContent.shift();
+            firstElement.hide();
+            divContent.push(firstElement);
+            var nextElement = divContent.shift();
+            nextElement.show(1000);                                               
+            divContent.push(nextElement);
+        }, 5000);
+    }
+
+    doHeadline();
+});
+
+
+
+
+
